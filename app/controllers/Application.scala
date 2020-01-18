64bc8b4b6b96f2d7b4dcbd00c4928f3a3fee82a9
niso1985 <niso1985@gmail.com>
2020-01-18 23:06:29 +0900

    add and apply scalariform

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -3,14 +3,11 @@ package controllers
 import com.stripe.Stripe
 import play.api.mvc._
 
-
 object Application extends Controller {
-
 
   Stripe.apiKey = "sk_test_NdLJjjEOj59wG3YueEeohJ0y00ipviQfOy"
 
-  def setup = Action{
-
+  def setup = Action {
 
     Ok("setup test")
   }
