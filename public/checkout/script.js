02c0d877305e67cf90453b7c2e5508306acee5c3
niso1985 <niso1985@gmail.com>
2020-01-28 00:52:42 +0900

    update: apply plan selection in front

--- public/checkout/script.js
+++ public/checkout/script.js
@@ -1,5 +1,5 @@
 // Create a Checkout Session with the selected plan ID
-var createCheckoutSession = function(name_, email_, village_) {
+var createCheckoutSession = function(name_, email_, village_, plan_) {
   return fetch("/create-checkout-session", {
     method: "POST",
     headers: {
@@ -8,7 +8,8 @@ var createCheckoutSession = function(name_, email_, village_) {
     body: JSON.stringify({
       name: name_,
       email: email_,
-      village: village_
+      village: village_,
+      plan: plan_
     })
   }).then(handleResult);
 };
@@ -17,7 +18,7 @@ var createCheckoutSession = function(name_, email_, village_) {
 var handleResult = function(result) {
   if (result.error) {
     var displayError = document.getElementById("error-message");
-    displayError.textContent = result.error.message;
+    displayError.textContent = "エラーが発生しました: " + result.error.message;
   } else {
     return result.json();
   }
@@ -31,15 +32,33 @@ fetch("/setup")
   .then(function(json) {
     var publicKey = json.publicKey;
     var stripe = Stripe(publicKey);
+    var plan1 = json.plan1;
+    var plan2 = json.plan2;
 
     // Setup event handler to create a Checkout Session when button is clicked
     document
-      .getElementById("checkout-button")
+      .getElementById("checkout-button-1")
       .addEventListener("click", function(evt) {
         var name = document.getElementById("name-input").value
         var email = document.getElementById("email-input").value
         var village = document.getElementById("village-input").value
-        createCheckoutSession(name, email, village).then(function(data) {
+        createCheckoutSession(name, email, village, plan1).then(function(data) {
+          // Call Stripe.js method to redirect to the new Checkout page
+          stripe
+            .redirectToCheckout({
+              sessionId: data.sessionId
+            })
+            .then(handleResult);
+        });
+      });
+
+    document
+      .getElementById("checkout-button-2")
+      .addEventListener("click", function(evt) {
+        var name = document.getElementById("name-input").value
+        var email = document.getElementById("email-input").value
+        var village = document.getElementById("village-input").value
+        createCheckoutSession(name, email, village, plan2).then(function(data) {
           // Call Stripe.js method to redirect to the new Checkout page
           stripe
             .redirectToCheckout({
